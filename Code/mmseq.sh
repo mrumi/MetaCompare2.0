c35b14d2153f52ef1f5a4ee094557494125d0909
Monjura Afrin Rumi <monjura.afrin@gmail.com>
2022-08-18 23:49:25 -0400

    Update mmseq.sh

--- Code/mmseq.sh
+++ Code/mmseq.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
-DB='/home/lqzhang/mmseq/GTDB/gtdb'
-DB='/home/lqzhang/mmseq/pdb/swissprot'
+DB='BlastDB/pdb/swissprot'
 
-echo ${1}
-echo ${2}
-echo ${3}
-
-
-
-/home/lqzhang/mmseq/MMseqs2/build/bin/mmseqs createdb ${1} ${2}sample.contigs
-/home/lqzhang/mmseq/MMseqs2/build/bin/mmseqs taxonomy ${2}sample.contigs $DB ${2}sample.assignments ${2}sample.tmpFolder --tax-lineage 1 --majority 0.7 --vote-mode 1 --lca-mode 3 --orf-filter 1
-/home/lqzhang/mmseq/MMseqs2/build/bin/mmseqs createtsv ${2}sample.contigs ${2}sample.assignments ${2}${3}
+mmseqs createdb ${1} ${2}sample.contigs
+mmseqs taxonomy ${2}sample.contigs $DB ${2}sample.assignments ${2}sample.tmpFolder --tax-lineage 1 --majority 0.7 --vote-mode 1 --lca-mode 3 --orf-filter 1
+mmseqs createtsv ${2}sample.contigs ${2}sample.assignments ${2}${3}
 rm ${2}sample.contigs*
 rm ${2}sample.assignments*
 rm -r ${2}sample.tmpFolder
-
-
-
