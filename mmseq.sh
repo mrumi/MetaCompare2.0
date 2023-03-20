58a5ea5581918a34f8eaf61a8a4353164ade5b1e
Monjura Afrin Rumi <monjura.afrin@gmail.com>
2023-03-19 20:39:44 -0400

    Update mmseq.sh

--- mmseq.sh
+++ mmseq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DB='BlastDB/pdb/swissprot'
+DB='metacmpDB/GTDB/gtdb'
 
 mmseqs createdb ${1} ${2}sample.contigs
 mmseqs taxonomy ${2}sample.contigs $DB ${2}sample.assignments ${2}sample.tmpFolder --tax-lineage 1 --majority 0.7 --vote-mode 1 --lca-mode 3 --orf-filter 1
