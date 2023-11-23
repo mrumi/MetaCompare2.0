d7cec5fec5adab7debc23e49bc67acc2102079f7
Monjura Afrin Rumi <monjura.afrin@gmail.com>
2023-11-23 11:16:28 -0500

    Update mmseq.sh

--- mmseq.sh
+++ mmseq.sh
@@ -5,6 +5,6 @@ DB='metacmpDB/GTDB/gtdb'
 mmseqs createdb ${1} ${2}sample.contigs
 mmseqs taxonomy ${2}sample.contigs $DB ${2}sample.assignments ${2}sample.tmpFolder --tax-lineage 1 --majority 0.7 --vote-mode 1 --lca-mode 3 --orf-filter 1
 mmseqs createtsv ${2}sample.contigs ${2}sample.assignments ${2}${3}
-rm ${2}sample.contigs*
-rm ${2}sample.assignments*
-rm -r ${2}sample.tmpFolder
+#rm ${2}sample.contigs*
+#rm ${2}sample.assignments*
+#rm -r ${2}sample.tmpFolder
