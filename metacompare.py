bb9e2930e5104ad441917e9d2894d8f0ae820b5d
Monjura Afrin Rumi <monjura.afrin@gmail.com>
2023-11-23 11:02:27 -0500

    Update metacompare.py

--- metacompare.py
+++ metacompare.py
@@ -1,10 +1,11 @@
 from sys import argv
 from sys import exit
 import os
+import pandas as pd
+
 
 from annotation import generate_annotation, process_annotation
 from calculation import calculate_score
-from visualization import visual_file_creation
 
 def getopts(argv):
 	opts = {}  # Empty dictionary to store key-value pairs.
@@ -45,27 +46,27 @@ if __name__ == '__main__':
 	annotated_data = generate_annotation(myargs['-c'], myargs['-o'], nthread)	
 	
 	if pipeline == 1:		
-		data_to_be_processed = [annotated_data[0], annotated_data[1], annotated_data[2]]
+		data_to_be_processed = [annotated_data[1], annotated_data[2], annotated_data[3]]
 		pathogens = os.path.join(os.path.dirname(os.path.abspath(__file__)), "metacmpDB/pathogen_list.txt")
 		filtered_data = process_annotation(data_to_be_processed, mge_len_file, pathogens)
 		result = calculate_score(myargs['-c'], filtered_data, pipeline)
 	elif pipeline == 2:		
-		data_to_be_processed = [annotated_data[3], annotated_data[1], annotated_data[2]]
+		data_to_be_processed = [annotated_data[4], annotated_data[2], annotated_data[3]]
 		pathogens = os.path.join(os.path.dirname(os.path.abspath(__file__)), "metacmpDB/eskape.txt")
 		filtered_data = process_annotation(data_to_be_processed, mge_len_file, pathogens)
 		result = calculate_score(myargs['-c'], filtered_data, pipeline)
 	else:
-		data_to_be_processed = [annotated_data[0], annotated_data[1], annotated_data[2]]
+		data_to_be_processed = [annotated_data[1], annotated_data[2], annotated_data[3]]
 		pathogens = os.path.join(os.path.dirname(os.path.abspath(__file__)), "metacmpDB/pathogen_list.txt")
 		filtered_data_e = process_annotation(data_to_be_processed, mge_len_file, pathogens)
 		result_e = calculate_score(myargs['-c'], filtered_data_e, 1)
 		
-		data_to_be_processed = [annotated_data[3], annotated_data[1], annotated_data[2]]
+		data_to_be_processed = [annotated_data[4], annotated_data[2], annotated_data[3]]
 		pathogens = os.path.join(os.path.dirname(os.path.abspath(__file__)), "metacmpDB/eskape.txt")
 		filtered_data_h = process_annotation(data_to_be_processed, mge_len_file, pathogens)
 		result_h = calculate_score(myargs['-c'], filtered_data_h, 2)
 		
-		result = result_e.append(result_h)
+		result = pd.concat([result_e, result_h])
 	
 	result.to_csv(out_file, header=True, index=None, sep = "\t")
 			
